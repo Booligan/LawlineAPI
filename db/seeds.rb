202d15f36f8c9180da972de093523caa7e8e5bc1
alberto <github email address>
2018-02-19 19:01:34 -0500

    added admin to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
-5.times do
+# Admin User seeded to DB
+
+User.create(first_name:"admin", last_name:"admin", email: "admin@lawline.com")
+
+# 4 more users seeded to DB
+4.times do
     User.create({
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
@@ -6,6 +11,7 @@
     })
 end
 
+# 10 products seeded to DB
 10.times do
     Product.create({
         name: Faker::Coffee.blend_name,
